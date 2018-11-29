8645d6ba9579b02ebf601662129291f36b903ca6
ZhenyaLysenko <lysenko.hp@gmail.com>
2018-11-29 17:56:49 +0200

    add Steps

--- js/tabs_in.js
+++ js/tabs_in.js
@@ -18,3 +18,13 @@ $(document).ready(function() {
 	})(jQuery);
 
 });
+
+var swiper = new Swiper('.swiper-container', {
+		 slidesPerView: 4,
+		 spaceBetween: 30,
+		 centeredSlides: true,
+		 pagination: {
+			 el: '.swiper-pagination',
+			 clickable: true,
+		 },
+	 });
