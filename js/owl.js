53d6b517bb504fef3f224b3b6ab08b983a5d0dea
ZhenyaLysenko <lysenko.hp@gmail.com>
2018-12-04 16:15:40 +0200

    add WhatInclude

--- js/owl.js
+++ js/owl.js
@@ -3,4 +3,19 @@ $('.owl-carousel').owlCarousel({
   center: true,
   loop: true,
   nav: true,
+  center: true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:1,
+          nav:true
+      },
+      1000:{
+          items:3,
+          nav:true,
+      }
+  }
 });
