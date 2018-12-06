0253e42b61c78410e52db6657cc039e312095716
ZhenyaLysenko <lysenko.hp@gmail.com>
2018-12-06 16:42:29 +0200

    fix styles, add js bg

---
+++ js/bg.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  var mouseX, mouseY;
+  var ww = $( window ).width();
+  var wh = $( window ).height();
+  var traX, traY;
+  $(document).mousemove(function(e){
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = ((4 * mouseX) / 570) + -10;
+    traY = ((4 * mouseY) / 570) + -20;
+    console.log(traX);
+    $(".bgFor").css({"background-position": traX + "%" + traY + "%"});
+  });
+});
